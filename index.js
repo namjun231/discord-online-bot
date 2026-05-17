4fbe23e43342905e788d8c8412538b1301d43b4a
namjun231 <namjun2211@gmail.com>
2026-05-18 01:47:07 +0700

    clean commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates
+  ]
+});
+
+client.once('ready', () => {
+  console.log('Bot online');
+});
+
+client.on('messageCreate', async (message) => {
+
+  if (message.content === '!join') {
+
+    const vc = message.member.voice.channel;
+
+   
+
+    joinVoiceChannel({
+      channelId: vc.id,
+      guildId: vc.guild.id,
+      adapterCreator: vc.guild.voiceAdapterCreator,
+      selfDeaf: false
+    });
+
+  }
+
+});
+
+client.login(process.env.TOKEN);
